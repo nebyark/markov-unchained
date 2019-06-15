1b482297e65f0d628d971796816fae854f382c02
Benjamin Kray <benkray@gmail.com>
2019-06-15 13:33:18 -0700

    Pod install on example project

---
+++ Example/Pods/Target Support Files/MarkovUnchained/MarkovUnchained-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MarkovUnchained : NSObject
+@end
+@implementation PodsDummy_MarkovUnchained
+@end
---
+++ Example/Pods/Target Support Files/Pods-MarkovUnchained_Example/Pods-MarkovUnchained_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MarkovUnchained_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MarkovUnchained_Example
+@end
